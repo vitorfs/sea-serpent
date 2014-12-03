6eca3fa98c17db1bb122f7a1b175e39a3479f286
Vitor Freitas <vitorfs@gmail.com>
2014-12-03 12:41:31 -0200

    bugfix

--- seaserpent/core/views.py
+++ seaserpent/core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import Q
 from seaserpent.core.models import Product
 
 def home(request):
@@ -7,7 +8,7 @@ def home(request):
     if not order: order = ''
     if order.replace('-', '') not in ('product_key', 'name', 'price', 'updated_at', 'visited_at', 'status', 'price_difference', ):
         order = '-updated_at'
-    products = Product.objects.exclude(status='novo').exclude(price=0.0).order_by(order)
+    products = Product.objects.exclude(status='novo').exclude(Q(price__isnull=True) | Q(price=0.0)).order_by(order)
     paginator = Paginator(products, 100)
     page = request.GET.get('page')
     try:
